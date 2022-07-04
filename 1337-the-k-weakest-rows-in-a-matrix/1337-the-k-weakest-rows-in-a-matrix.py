b5829fda42f6fddb5f69431fa43a444b2c477e9b
timothyquan <me@timquan.ca>
2022-07-04 17:13:43 +0700

    Time: 173 ms (45.02%), Space: 14.3 MB (13.84%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,15 +1,8 @@
-#version 2: the first one I used numpy, which was 2 lines but ended up being in the bottom 5% for efficiency. 
-#here I attempt to use a list as a hash table to see if it is any faster. 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        solmap = [ [] for m in range(len(mat[0])+1)]
-        for idx,val in enumerate(mat):
-            solmap[sum(val)].append(idx)
-        weakest = []
-        i = 0
-        
-        print(solmap)
-        while len(weakest) < k:
-            weakest += solmap[i]
-            i+=1
-        return weakest[:k]    
+        solcnt = [sum(s) for s in mat]
+        weakcnt = set(sorted(solcnt)[:k])
+        weakidx = []
+        for wr in weakcnt:
+            weakidx += [i for i,r in enumerate(solcnt) if r == wr]
+        return weakidx[:k]
