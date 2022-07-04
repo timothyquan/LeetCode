03f10630feee7234fa6449762a375c3751c55eeb
timothyquan <me@timquan.ca>
2022-07-04 19:09:33 +0700

    Time: 47 ms (44.10%), Space: 13.8 MB (52.17%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -5,5 +5,5 @@ class Solution:
             count += 1
             if num % 2 == 0:
                 num = num / 2
-            else: num = num - 1
+            else: num -= 1
         return count    
