2f485331696ceb6f09c8574ac3d855b0f881b7c2
timothyquan <me@timquan.ca>
2022-07-06 14:14:42 +0700

    Time: 40 ms (94.51%), Space: 14.2 MB (89.00%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,9 +1,8 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         smap, tmap = {}, {}
-        for i in range(len(s)):
-            if not s[i] in smap.keys(): smap[s[i]] = i
-            if not t[i] in tmap.keys(): tmap[t[i]] = i
-            if not smap[s[i]] == tmap[t[i]]: return False
+        for i in range(len(s)):            
+            if not smap.setdefault(s[i], i) == tmap.setdefault(t[i], i):
+                return False
         return True
             
