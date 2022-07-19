c7cee7f0c6382c7184006d662940f26847dd5f5a
timothyquan <me@timquan.ca>
2022-07-19 17:33:49 +0700

    Time: 265 ms (83.47%), Space: 15.6 MB (22.24%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,18 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        idx = newidx = 1 
-        length = len(nums) - 1 
-        if target == nums[0]: return 0
-        elif target == nums[-1]: return length
-        while newidx < length:
-            newidx = idx * 2 if idx * 2 < length else length + 1
-            res = self.find(nums[idx:newidx], target)
-            if res != -1: return res + idx
-            else: idx = newidx
-        return -1    
+        left, right = 0, len(nums) -1        
+        
+        while right >= left:            
+            pivot = (right + left)//2
+            if nums[pivot] == target: return pivot
+            elif nums[pivot] > target: right = pivot - 1
+            elif nums[pivot] < target: left = pivot + 1
+
+        
                 
-                    
-    def find(self, nums, target) -> int:        
-        if target in nums:
-            return nums.index(target)        
-        else: return -1
+        return -1 
