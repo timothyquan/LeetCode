a88197a20d17a68c0ba2ae18d5005d527216312c
timothyquan <me@timquan.ca>
2022-08-04 08:49:37 +0700

    Time: 803 ms (33.68%), Space: 15 MB (77.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         for i,n in enumerate(nums):
             seek = target - n
             if seek in nums[i+1:]:
-                ret = [i, nums[i+1:].index(seek) + i + 1]
-        return ret
+                return [i, nums[i+1:].index(seek) + i + 1]
+      
                 
        
