30b10362cac3d0950cfd8b247b89219310993a0d
timothyquan <me@timquan.ca>
2022-08-05 13:07:15 +0700

    Time: 247 ms (18.28%), Space: 20.8 MB (29.70%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        bins = [bin(i)[2:] for i in range(n+1)]
-        res = []
-        for b in bins:
-            res.append(0)
-            for c in b:
-                if c == '1':
-                    res[-1] += 1
+        res = [] 
+        for i in range(n + 1):
+            res.append(self.hamming_weight(i))
         return res
-
+        
+    def hamming_weight(self, n:int) -> int:
+        res = 0
+        while n:
+            res += n % 2 
+            n = n >> 1
+        return res
