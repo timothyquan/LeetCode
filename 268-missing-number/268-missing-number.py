e4be732c701cc6b91fce7a0ed75507985a3aa1b2
timothyquan <me@timquan.ca>
2022-08-06 19:47:20 +0700

    Time: 193 ms (66.64%), Space: 15.3 MB (34.78%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        miss = set(nums)
-        full = {num for num in range(len(nums)+1)}
-        return full.difference(miss).pop()
+       # miss = set(nums)
+       # full = {num for num in range(len(nums)+1)}
+       # return full.difference(miss).pop()
+        res = len(nums)
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
+            
         
       
