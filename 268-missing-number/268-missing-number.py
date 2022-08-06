99ce55903d7f0d1f23162a52bb6b0321bb74d86e
timothyquan <me@timquan.ca>
2022-08-06 19:58:37 +0700

    Time: 223 ms (51.39%), Space: 15.4 MB (13.45%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -3,11 +3,14 @@ class Solution:
        # miss = set(nums)
        # full = {num for num in range(len(nums)+1)}
        # return full.difference(miss).pop()
-        res = len(nums)
-        for i in range(len(nums)):
-            res += i - nums[i]
-            print(res,i)
-        return res
+        
+       # res = len(nums)
+       # for i in range(len(nums)):
+       #     res += i - nums[i]
+       #     print(res,i)
+       # return res
+        
+        return sum(list(range(len(nums)+1))) - sum(nums)
             
         
       
