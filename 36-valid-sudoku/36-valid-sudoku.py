e7fe1cd28a57e4611b83f725cfbbe3e73b5f5629
timothyquan <me@timquan.ca>
2022-08-08 13:11:07 +0700

    Time: 152 ms (50.83%), Space: 14 MB (35.50%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,32 +1,33 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        cols = [['.'] * 9 for _ in range(9)] 
-        boards = [[] for _ in range(9)] 
-        board_idx = {(0,0): 0, (0,1): 1, (0,2): 2,
-                     (1,0): 3, (1,1): 4, (1,2): 5,
-                     (2,0): 6, (2,1): 7, (2,2): 8}
-        
-        for ri,r in enumerate(board):
-            if self.contains_duplicate(r): return False             
-            for ci,c in enumerate(r):
-                boards[board_idx[(ri//3,ci//3)]].append(c)
-                cols[ci][ri] = c
-                
-        for c in cols:
-            if self.contains_duplicate(c): return False
+        def contains_dups(l : dict):
             
-        for b in boards:
-            if self.contains_duplicate(b): return False
-            
-        return True
+            pass
+                    
+                    
+        cols = [{} for _ in range(9)]
+        subs = [{} for _ in range(9)]
         
-                
-                 
-        
-    def contains_duplicate(self, l: List[str])-> bool:
-        lm = {}
-        for c in l:
-            if not c == '.':
-                lm[c] = lm.get(c, -1) + 1
-        return any(lm.values())
+        for ri, r in enumerate(board):
+            subrow = ri // 3
+            row = {}
+            for ci, c in enumerate(r):                
+                subcol = ci // 3
+                subidx = (subrow * 3) + subcol
+                if c != '.':
+                    row[c] = row.get(c, -1) + 1
+                    cols[ci][c] = cols[ci].get(c, -1) + 1
+                    subs[subidx][c] = subs[subidx].get(c, -1) + 1
+                if ci == 8 and any(row.values()):
+                    return False
+                if ri == 8 and any(cols[ci].values()):
+                    print(cols[ci])
+                    return False
+                if ri % 3 == 2 and ci % 3 == 2 and any(subs[subidx].values()):                    
+                    return False
+        [print(r) for r in board]
+        return True        
+                    
+                    
+                    
