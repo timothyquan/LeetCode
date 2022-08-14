5be1d873c00d5ca371f0985760abad40c9944d77
timothyquan <me@timquan.ca>
2022-08-14 12:08:45 +0700

    Time: 1885 ms (33.33%), Space: 32 MB (16.67%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
@@ -3,8 +3,16 @@ class Solution:
         edgemap = {}
         for i,edge in enumerate(edges):
             edgemap[edge] = edgemap.get(edge, 0) + i
-        return sorted(edgemap.items(), key=lambda kv: (kv[1], -kv[0]))[-1][0]
-
+        #return sorted(edgemap.items(), key=lambda kv: (kv[1], -kv[0]))[-1][0]
+        
+        highkey, highval = 0, 0
+        for kv in edgemap.items():
+            if kv[1] > highval:
+                highkey, highval = kv
+            elif kv[1] == highval and highkey > kv[0]:
+                highkey, highval = kv 
+                
+        return highkey
 
             
             
