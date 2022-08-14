a572933f07769ed06eccd483cdd7be4505cd7c37
timothyquan <me@timquan.ca>
2022-08-14 12:16:29 +0700

    Time: 1549 ms (33.33%), Space: 31.8 MB (16.67%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
@@ -1,15 +1,18 @@
 class Solution:
     def edgeScore(self, edges: List[int]) -> int:
         edgemap = {}
-        edgeheap = []
-        heapq.heapify(edgeheap)
         for i,edge in enumerate(edges):
             edgemap[edge] = edgemap.get(edge, 0) + i
-            heapq.heappush(edgeheap, (-edgemap[edge], edge))
         #return sorted(edgemap.items(), key=lambda kv: (kv[1], -kv[0]))[-1][0]
         
+        highkey, highval = 0, 0
+        for kv in edgemap.items():
+            if kv[1] > highval:
+                highkey, highval = kv
+            elif kv[1] == highval and highkey > kv[0]:
+                highkey, highval = kv 
                 
-        return heapq.heappop(edgeheap)[1]
+        return highkey
 
             
             
